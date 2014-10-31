185289bc0949e4dc3e9443704b99a015fdd55739
Steve <mctyler13@gmail.com>
2014-10-31 12:55:34 -0500

    Update and rename src/main/java/net/coasterman10/Annihilation/object/Nexus.java to src/main/java/org/eodsteven/CrafterNexus/object/Nexus.java

--- src/main/java/org/eodsteven/CrafterNexus/object/Nexus.java
+++ src/main/java/org/eodsteven/CrafterNexus/object/Nexus.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  ******************************************************************************/
-package net.coasterman10.Annihilation.object;
+package org.eodsteven.CrafterNexus.object;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
